f2ebe4ac471cafc2173c237eba7ca70f6249053c
Arya Rezza Anantya <anantya4995@gmail.com>
2023-11-08 00:21:05 +0700

    Update PreferenceRepository.kt

--- app/src/main/java/id/anantyan/synrgychapter4/domain/PreferenceRepository.kt
+++ app/src/main/java/id/anantyan/synrgychapter4/domain/PreferenceRepository.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.flow.Flow
 interface PreferenceRepository {
     suspend fun setTheme(value: Boolean)
     fun getTheme(): Flow<Boolean>
-
     suspend fun setLogin(value: Boolean)
     fun getLogin(): Flow<Boolean>
     suspend fun setUsrId(value: Long)
